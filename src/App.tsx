e9231a36e1d63e756a9b9ca93d4b4554da425ac8
joooons <52220710+joooons@users.noreply.github.com>
2023-12-02 18:17:48 -0800

    highlight selected list item

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-
-
-// import Message from './Message';
-import ListGroup  from './components/ListGroup';
+import ListGroup from './components/ListGroup';
 
 function App() {
   return (
     <div>
-      <ListGroup/>
+      <ListGroup />
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,7 +1,30 @@
-
+import { useState } from 'react';
 
 function ListGroup() {
-    return <h1>List group</h1>;
+  const category = 'Numbers';
+  const monsters = ['Yipper', 'Pupper', 'Borker', 'Woofer'];
+  const [monsterIndex, setMonsterIndex] = useState(-1);
+  return (
+    <>
+      <h1 className='list-group-title'>List of {category}</h1>
+      <ul className='list-group'>
+        {monsters.length === 0 && <p>Nothing to see</p>}
+        {monsters.map((monster, index) => (
+          <li
+            key={index}
+            className={
+              index === monsterIndex
+                ? 'list-group-item active'
+                : 'list-group-item'
+            }
+            onClick={() => setMonsterIndex(index)}
+          >
+            {monster}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
