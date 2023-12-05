6655c7442da62e9655d3052a38c557695595e2a5
joooons <52220710+joooons@users.noreply.github.com>
2023-12-04 23:12:03 -0800

    add css for Card & Input

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Input from './Input';
+import './Card.css';
 
 const Card = () => {
   const [username, setUsername] = useState('CLICK');
@@ -12,8 +13,8 @@ const Card = () => {
     <>
       <div className='card m-5 p-3'>
         <div className='card-body'>
-          <h5 className='card-title'>Do you have a name?</h5>
-          <p className='card-text'>Enter it here!</p>
+          <h5 className='card-title'>Wanna say something?</h5>
+          <p className='card-text'>Go ahead!</p>
           <Input username={username} updateName={updateName}></Input>
           <p className='card-text'>{username}</p>
         </div>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,3 +1,5 @@
+import './Input.css';
+
 interface Props {
   username: string;
   updateName: (event: Event) => void;
@@ -11,10 +13,14 @@ const Input = ({ username, updateName }: Props) => {
           type='text'
           value={username}
           className='form-control'
-          placeholder='Enter an idea'
-          aria-label='Enter an idea'
+          placeholder='Write something'
+          aria-label='Write something'
           aria-describedby='basic-addon1'
-          onChange={(ev) => updateName(ev)}
+          onChange={(ev: React.ChangeEvent) => {
+            const thing = ev as unknown;
+            const event = thing as Event;
+            updateName(event);
+          }}
         />
       </div>
     </>
