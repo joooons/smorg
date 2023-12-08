ebcc73a985bc5ace82461cd96ace3af85f6b4d9d
joooons <52220710+joooons@users.noreply.github.com>
2023-12-07 22:16:44 -0800

    links open new tabs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,9 +7,28 @@ const Home = () => {
     <>
       <Container>
         <Text tag='h1'>This is the Home Page</Text>
-        <Text>
-          <Link to='/space'>Wanna try this thing out?</Link>
-        </Text>
+        <ul style={{ listStyleType: 'none' }}>
+          <li>
+            <Link
+              to='/space'
+              target='_blank'
+              rel='noopener noreferrer'
+              style={{ textDecoration: 'none' }}
+            >
+              SPACE
+            </Link>
+          </li>
+          <li>
+            <Link
+              to='/about'
+              target='_blank'
+              rel='noopener noreferrer'
+              style={{ textDecoration: 'none' }}
+            >
+              ABOUT
+            </Link>
+          </li>
+        </ul>
       </Container>
     </>
   );
