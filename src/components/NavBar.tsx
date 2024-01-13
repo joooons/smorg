c669296d3c4ab3eb291921a196ff892fbdd17b4f
joooons <52220710+joooons@users.noreply.github.com>
2024-01-12 22:58:29 -0800

    try navbar in about page

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,45 @@
+import './NavBar.css';
+
+const NavBar = () => {
+  return (
+    <>
+      <nav className='navbar navbar-expand-lg navbar-light bg-warning px-5 py-3'>
+        <a className='navbar-brand' href='/'>
+          SMORG
+        </a>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-toggle='collapse'
+          data-target='#navbarSupportedContent'
+          aria-controls='navbarSupportedContent'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span className='navbar-toggler-icon'></span>
+        </button>
+        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+          <ul className='navbar-nav mr-auto'>
+            <li className='nav-item active'>
+              <a className='nav-link' href='#'>
+                Home
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='/space'>
+                Space
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='#'>
+                Monkey
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default NavBar;
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,15 @@
 import { Link } from 'react-router-dom';
 import Container from '../components/Container';
 import Text from '../components/Text';
+import NavBar from '../components/NavBar';
 
 const About = () => {
   return (
     <>
+      {/* <Container> */}
+      <NavBar></NavBar>
+      {/* </Container> */}
+
       <Container>
         <Text color='navy' size='50' font='winter' tag='h1'>
           This page is ABOUT to be filled out, but not quite yet.
