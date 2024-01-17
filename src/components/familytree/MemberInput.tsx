8ebb97381f0d669584da136e9f5cfa24f7fbccbb
joooons <52220710+joooons@users.noreply.github.com>
2024-01-16 22:15:04 -0800

    clean up Tree.tsx, inert MemberInput

--- src/components/familytree/MemberInput.tsx
+++ src/components/familytree/MemberInput.tsx
@@ -1,21 +1,16 @@
-import React, { ReactNode, useState } from 'react';
 import './MemberInput.css';
 
 interface Props {
   val: string;
   addMember?: (event: Event) => void;
-  updateMember: (event: Event) => void;
+  updateMember?: (event: Event) => void;
 }
 
-const MemberInput = ({ val, updateMember }: Props) => {
+const MemberInput = ({ val }: Props) => {
   return (
     <>
       <div className='member-input m-1 px-1'>
-        <input
-          onChange={updateMember}
-          //   placeholder={val}
-          value={val}
-        ></input>
+        <input value={val}></input>
       </div>
     </>
   );
--- src/pages/Tree.tsx
+++ src/pages/Tree.tsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
 
 import Container from '../components/Container';
 import Text from '../components/Text';
