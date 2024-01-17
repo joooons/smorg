651c7f6c207075a5ef743f5ab1de1db74af72c1e
joooons <52220710+joooons@users.noreply.github.com>
2024-01-17 15:30:05 -0800

    add Button but without function yet

--- src/pages/Tree.tsx
+++ src/pages/Tree.tsx
@@ -2,16 +2,21 @@ import Container from '../components/Container';
 import Text from '../components/Text';
 import TextArea from '../components/TextArea';
 import NavBar from '../components/NavBar';
+import Centered from '../components/Centered';
+import Button from '../components/Button';
 
 const Tree = () => {
   return (
     <>
       <NavBar></NavBar>
       <Container>
-        <Text color='navy' size='50' font='winter' tag='h1'>
-          TREE
-        </Text>
+        <Centered>
+          <Text color='navy' size='50' font='winter' tag='h1'>
+            TREE
+          </Text>
+        </Centered>
         <TextArea></TextArea>
+        <Button>GENERATE</Button>
       </Container>
     </>
   );
