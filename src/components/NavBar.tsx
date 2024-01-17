f352b684afe1b62a794d0c34ba5884fac9251c26
joooons <52220710+joooons@users.noreply.github.com>
2024-01-16 22:19:29 -0800

    mod sequence of navbar items

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -20,24 +20,19 @@ const NavBar = () => {
         </button>
         <div className='collapse navbar-collapse' id='navbarSupportedContent'>
           <ul className='navbar-nav mr-auto'>
-            <li className='nav-item active'>
-              <a className='nav-link' href='#'>
-                Home
-              </a>
-            </li>
             <li className='nav-item'>
               <a className='nav-link' href='/space'>
                 Space
               </a>
             </li>
             <li className='nav-item'>
-              <a className='nav-link' href='/about'>
-                About
+              <a className='nav-link' href='/tree'>
+                Tree
               </a>
             </li>
             <li className='nav-item'>
-              <a className='nav-link' href='/tree'>
-                Tree
+              <a className='nav-link' href='/about'>
+                About
               </a>
             </li>
           </ul>
