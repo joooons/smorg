07bdd3d38fecce3c12e83399fa1769accd97e7e1
joooons <52220710+joooons@users.noreply.github.com>
2024-01-17 23:56:03 -0800

    use SteppedLineTo from react-lineto npm package

--- src/components/familytree/TreeNodeComponent.tsx
+++ src/components/familytree/TreeNodeComponent.tsx
@@ -1,6 +1,7 @@
 // import React from 'react';
 
 import { TreeNode } from './TreeNodeInterface';
+import { SteppedLineTo } from 'react-lineto';
 
 import './TreeNodeComponent.css';
 
@@ -9,31 +10,62 @@ interface Props {
   color?: string;
 }
 
-const colors = ['rgb(198, 255, 225)', 'rgb(233, 255, 198)'];
+const colors = ['lightgreen', 'lightgreen'];
+// const colors = ['rgb(230, 255, 235)', 'rgb(240, 255, 210)'];
+// const colors = ['rgb(240, 250, 240)', 'rgb(250, 250, 230)'];
 
 const TreeNodeComponent = ({ node, color = colors[0] }: Props) => {
   const color2 = color == colors[0] ? colors[1] : colors[0];
   return (
     <div
       style={{
-        backgroundColor: `${color}`,
         textAlign: 'center',
         verticalAlign: 'top',
-        padding: '3px',
-        borderRadius: '10px',
         margin: '2px',
         display: 'inline-block',
       }}
     >
-      <div>
-        {node.name}
-        {node.spouse && ' & ' + node.spouse}
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <div
+          className={node.id.toString()}
+          style={{
+            backgroundColor: `${color}`,
+            padding: '0px 5px 0px 5px',
+            borderRadius: '5px',
+          }}
+        >
+          {node.name}
+          {node.spouse && ' & ' + node.spouse}
+        </div>
       </div>
+
+      <div className='blank-space'></div>
       <div>
         {node.children && (
           <div>
             {node.children.map((child) => (
               <TreeNodeComponent key={child.id} node={child} color={color2} />
+            ))}
+          </div>
+        )}
+        {node.children && (
+          <div>
+            {node.children.map((child) => (
+              <SteppedLineTo
+                key={child.id}
+                from={node.id.toString()}
+                to={child.id.toString()}
+                borderColor='black'
+                borderWidth={2}
+                fromAnchor='bottom center'
+                toAnchor='top center'
+              />
             ))}
           </div>
         )}
