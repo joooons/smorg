4e1c7691bb4896df530f04f60e8874b859fdab53
joooons <52220710+joooons@users.noreply.github.com>
2024-01-23 23:06:29 -0800

    bring back multiple cards in Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,11 +6,11 @@ import LogoBox from '../components/LogoBox';
 import Centered from '../components/Centered';
 
 import spaceLogo from '../../src/assets/space.png';
-// import aboutLogo from '../../src/assets/about.png';
-// import crashGoLogo from '../../src/assets/crashGo.png';
-// import loveLogo from '../../src/assets/love.png';
-// import mazeLogo from '../../src/assets/maze.png';
-// import monsterLogo from '../../src/assets/monster.png';
+import aboutLogo from '../../src/assets/about.png';
+import crashGoLogo from '../../src/assets/crashGo.png';
+import loveLogo from '../../src/assets/love.png';
+import mazeLogo from '../../src/assets/maze.png';
+import monsterLogo from '../../src/assets/monster.png';
 import treeLogo from '../../src/assets/tree.png';
 import smorgLogo from '../../src/assets/smorg-b.png';
 
@@ -47,6 +47,56 @@ const Home = () => {
               <Link to='/tree'>
                 <LogoBox>
                   <Logo image={treeLogo}></Logo>
+                </LogoBox>
+              </Link>
+            </Centered>
+          </div>
+
+          <div className='col-sm-6 col-md-4 col-xl-3 col-xxl-2 mt-3'>
+            <Centered>
+              <Link to='/about'>
+                <LogoBox>
+                  <Logo image={aboutLogo}></Logo>
+                </LogoBox>
+              </Link>
+            </Centered>
+          </div>
+
+          <div className='col-sm-6 col-md-4 col-xl-3 col-xxl-2 mt-3'>
+            <Centered>
+              <Link to='/chickens'>
+                <LogoBox>
+                  <Logo image={crashGoLogo}></Logo>
+                </LogoBox>
+              </Link>
+            </Centered>
+          </div>
+
+          <div className='col-sm-6 col-md-4 col-xl-3 col-xxl-2 mt-3'>
+            <Centered>
+              <Link to='/chickens'>
+                <LogoBox>
+                  <Logo image={loveLogo}></Logo>
+                </LogoBox>
+              </Link>
+            </Centered>
+          </div>
+
+          <div className='col-sm-6 col-md-4 col-xl-3 col-xxl-2 mt-3'>
+            <Centered>
+              <Link to='/chickens'>
+                <LogoBox>
+                  <Logo image={mazeLogo}></Logo>
+                </LogoBox>
+              </Link>
+            </Centered>
+          </div>
+
+          <div className='col-sm-6 col-md-4 col-xl-3 col-xxl-2 mt-3'>
+            <Centered>
+              <Link to='/chickens'>
+                <LogoBox>
+                  <Logo image={monsterLogo}></Logo>
                 </LogoBox>
               </Link>
             </Centered>
