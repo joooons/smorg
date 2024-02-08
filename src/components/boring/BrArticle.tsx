b9705d71bd440f2a541c47d378697460dfeb8622
joooons <52220710+joooons@users.noreply.github.com>
2024-02-08 14:17:41 -0800

    add Props to BrArticle.tsx

--- src/components/boring/BrArticle.tsx
+++ src/components/boring/BrArticle.tsx
@@ -4,12 +4,28 @@ import './BrArticle.css';
 
 interface Props {
   children: ReactNode;
+  image?: string;
+  caption?: string;
+  description?: string;
+  writer?: string;
 }
 
-const BrArticle = ({ children }: Props) => {
+const BrArticle = ({
+  image,
+  caption,
+  children,
+  description,
+  writer,
+}: Props) => {
   return (
     <>
-      <div className='br-article'>{children}</div>
+      <div className='br-article'>
+        <div>{image}</div>
+        <div>{caption}</div>
+        <div>{children}</div>
+        <div>{description}</div>
+        <div>{writer}</div>
+      </div>
     </>
   );
 };
--- src/pages/Boring.tsx
+++ src/pages/Boring.tsx
@@ -7,13 +7,34 @@ function Boring() {
       <Container>
         <div className='row'>
           <div className='col-sm-4'>
-            <BrArticle>Taylor Swift something</BrArticle>
+            <BrArticle
+              image='image.png'
+              caption='blank image'
+              description='She makes history for winning ALL awards at the Grannys'
+              writer='sarah sarahson'
+            >
+              Taylor Swift something
+            </BrArticle>
           </div>
           <div className='col-sm-4'>
-            <BrArticle>Someone died</BrArticle>
+            <BrArticle
+              image='image.png'
+              caption='blank image'
+              description='Two found dead at underground pillow fight scene'
+              writer='sarah sarahson'
+            >
+              Killing Me Softly
+            </BrArticle>
           </div>
           <div className='col-sm-4'>
-            <BrArticle>Netflix causes cancer</BrArticle>
+            <BrArticle
+              image='image.png'
+              caption='blank image'
+              description='Research suggests correlation between tooth decay and horror genre'
+              writer='sarah sarahson'
+            >
+              Netflix causes cavities
+            </BrArticle>
           </div>
         </div>
       </Container>
