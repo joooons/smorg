f4b362e6ed34a2cd1a56297be5438d6e1e132928
joooons <52220710+joooons@users.noreply.github.com>
2024-02-14 17:21:07 -0800

    remove Container from Tree.tsx

--- src/pages/Tree.tsx
+++ src/pages/Tree.tsx
@@ -2,18 +2,17 @@ import React, { useState, useEffect, useRef } from 'react';
 import { useXarrow, Xwrapper } from 'react-xarrows';
 
 // COMPONENTS
-import Container from '../components/Container';
 import Text from '../components/Text';
 import TextArea from '../components/TextArea';
 import NavBar from '../components/NavBar';
 import Centered from '../components/Centered';
 import Button from '../components/Button';
+import FullHeight from '../components/familytree/FullHeight';
 import TreeNodeComponent from '../components/familytree/TreeNodeComponent';
 import TreeLineComponent from '../components/familytree/TreeLineComponent';
 
-// CLASS and INTERFACE
+// CLASSES
 import { TreeNode } from '../components/familytree/TreeNodeClass';
-import FullHeight from '../components/familytree/FullHeight';
 
 const Tree = () => {
   const initialText =
@@ -170,43 +169,41 @@ const Tree = () => {
   return (
     <>
       <NavBar></NavBar>
-      <Container>
-        <div className='row'>
+      <div className='row'>
+        <Centered>
+          <Text color='navy' size='50' font='winter' tag='h1'>
+            TREE
+          </Text>
+        </Centered>
+      </div>
+      <div className='row mx-5'>
+        <div className='col-sm-3'>
+          <TextArea
+            text={textareaText}
+            keydownMethod={handleKeyDown}
+            changeMethod={handleChange}
+          ></TextArea>
           <Centered>
-            <Text color='navy' size='50' font='winter' tag='h1'>
-              TREE
-            </Text>
+            <Button value={textareaText} action={fillTree}>
+              GENERATE
+            </Button>
           </Centered>
         </div>
-        <div className='row'>
-          <div className='col-sm-3'>
-            <TextArea
-              text={textareaText}
-              keydownMethod={handleKeyDown}
-              changeMethod={handleChange}
-            ></TextArea>
-            <Centered>
-              <Button value={textareaText} action={fillTree}>
-                GENERATE
-              </Button>
-            </Centered>
-          </div>
-          <div className='col-sm-9'>
-            <Xwrapper>
-              <FullHeight>
-                <Centered>
-                  <React.Fragment key={'node-' + point.id}>
-                    <TreeNodeComponent node={point}></TreeNodeComponent>
-                  </React.Fragment>
-                </Centered>
-              </FullHeight>
-              <React.Fragment key={'line-' + point.id}>
-                <TreeLineComponent node={point}></TreeLineComponent>
-              </React.Fragment>
-            </Xwrapper>
-          </div>
+        <div className='col-sm-9'>
+          <Xwrapper>
+            <FullHeight>
+              <Centered>
+                <React.Fragment key={'node-' + point.id}>
+                  <TreeNodeComponent node={point}></TreeNodeComponent>
+                </React.Fragment>
+              </Centered>
+            </FullHeight>
+            <React.Fragment key={'line-' + point.id}>
+              <TreeLineComponent node={point}></TreeLineComponent>
+            </React.Fragment>
+          </Xwrapper>
         </div>
-      </Container>
+      </div>
     </>
   );
 };
