9a599c63bc4a0a7db64e702e97477d1073f19883
joooons <52220710+joooons@users.noreply.github.com>
2024-02-14 23:40:50 -0800

    try Contentful

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_CONTENTFUL_ACCESS_TOKEN: string;
+    // Add more env variables as needed
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+  
