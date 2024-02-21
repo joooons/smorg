59535f933bb10ddbbb919171059f75ad44dc1309
joooons <52220710+joooons@users.noreply.github.com>
2024-02-20 23:05:03 -0800

    abstract out all of footer to BrFooter.tsx

--- src/components/notnews/BrFooter.tsx
+++ src/components/notnews/BrFooter.tsx
@@ -1,15 +1,142 @@
-import { ReactNode } from 'react';
-
 import './BrFooter.css';
+import { Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-interface Props {
-  children: ReactNode;
-}
+import {
+  FaSquareXTwitter,
+  FaSquareInstagram,
+  FaSquareReddit,
+  FaSquarePinterest,
+  FaSquareFacebook,
+  FaSquareYoutube,
+  FaSquareTumblr,
+} from 'react-icons/fa6';
 
-const BrFooter = ({ children }: Props) => {
+const BrFooter = () => {
   return (
     <>
-      <div className='br-footer py-4'>{children}</div>
+      <div className='br-footer py-4'>
+        <Container fluid='lg'>
+          <div className='row pt-4 '>
+            <div className='col-md-6 col-lg-2 mb-4'>
+              <div className='br-footer-item-heading'>About</div>
+              <div>
+                <Link to='/notnews' className='br-link'>
+                  BNN
+                </Link>
+              </div>
+              <div>
+                <Link to='/notnews' className='br-link'>
+                  Careers
+                </Link>
+              </div>
+            </div>
+
+            <div className='col-md-6 col-lg-2 mb-4'>
+              <div className='br-footer-item-heading'>Contact</div>
+              <div>
+                <Link to='/notnews' className='br-link'>
+                  Customer Center
+                </Link>
+              </div>
+              <div>
+                <Link to='/notnews' className='br-link'>
+                  Contact Us
+                </Link>
+              </div>
+              <div>
+                <Link to='/notnews' className='br-link'>
+                  Press
+                </Link>
+              </div>
+            </div>
+
+            <div className='col-md-6 col-lg-7 mb-4'>
+              <div className='br-footer-item-heading'>Subscription</div>
+              <div>
+                <Link to='/notnews' className='br-link'>
+                  Manage Subscriptions
+                </Link>
+              </div>
+              <div>
+                <Link to='/notnews' className='br-link'>
+                  Podcast
+                </Link>
+              </div>
+              <div>
+                <Link to='/notnews' className='br-link'>
+                  Newsletter
+                </Link>
+              </div>
+            </div>
+
+            <div className='col-sm-2 col-md-2 col-lg-1 mb-4'>
+              <div className='br-footer-item-heading'>Follow</div>
+              <div className='row'>
+                <div className='col-1'>
+                  <Link to='/notnews' className='br-link'>
+                    <FaSquareXTwitter />
+                  </Link>
+                </div>
+                <div className='col-1'>
+                  <Link to='/notnews' className='br-link'>
+                    <FaSquareInstagram />
+                  </Link>
+                </div>
+                <div className='col-1'>
+                  <Link to='/notnews' className='br-link'>
+                    <FaSquareFacebook />
+                  </Link>
+                </div>
+                <div className='col-1'>
+                  <Link to='/notnews' className='br-link'>
+                    <FaSquareYoutube />
+                  </Link>
+                </div>
+                <div className='col-1'>
+                  <Link to='/notnews' className='br-link'>
+                    <FaSquareTumblr />
+                  </Link>
+                </div>
+                <div className='col-1'>
+                  <Link to='/notnews' className='br-link'>
+                    <FaSquarePinterest />
+                  </Link>
+                </div>
+                <div className='col-1'>
+                  <Link to='/notnews' className='br-link'>
+                    <FaSquareReddit />
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='row pt-3 pb-5 '>
+            <div className='row'>
+              <div className='col-12'>
+                <div className='br-footer-row pt-3'>
+                  <Link to='/notnews' className='br-link br-footer-item'>
+                    Privacy Policy
+                  </Link>
+                  <Link to='/notnews' className='br-link br-footer-item'>
+                    Do Not Sell Or Share My Personal Information
+                  </Link>
+                  <Link to='/notnews' className='br-link br-footer-item'>
+                    Terms & Conditions
+                  </Link>
+                  <Link to='/notnews' className='br-link br-footer-item'>
+                    Terms of Sale
+                  </Link>
+                  <Link to='/notnews' className='br-link br-footer-item'>
+                    Site Map
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Container>
+      </div>
     </>
   );
 };
--- src/pages/NotNews.tsx
+++ src/pages/NotNews.tsx
@@ -1,19 +1,8 @@
 // npm packages
 import { useEffect, useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { createClient } from 'contentful';
-
-// icons
-import {
-  FaSquareXTwitter,
-  FaSquareInstagram,
-  FaSquareReddit,
-  FaSquarePinterest,
-  FaSquareFacebook,
-  FaSquareYoutube,
-  FaSquareTumblr,
-} from 'react-icons/fa6';
 
 // notnews components
 import BrArticle from '../components/notnews/BrArticle';
@@ -189,128 +178,7 @@ function NotNews() {
         </Row>
         <Row className='py-4'></Row>
       </Container>
-      <BrFooter>
-        <Container fluid='lg'>
-          <div className='row pt-4 '>
-            <div className='col-md-6 col-lg-2 mb-4'>
-              <div className='br-footer-item-heading'>About</div>
-              <div>
-                <Link to='/notnews' className='br-link'>
-                  BNN
-                </Link>
-              </div>
-              <div>
-                <Link to='/notnews' className='br-link'>
-                  Careers
-                </Link>
-              </div>
-            </div>
-
-            <div className='col-md-6 col-lg-2 mb-4'>
-              <div className='br-footer-item-heading'>Contact</div>
-              <div>
-                <Link to='/notnews' className='br-link'>
-                  Customer Center
-                </Link>
-              </div>
-              <div>
-                <Link to='/notnews' className='br-link'>
-                  Contact Us
-                </Link>
-              </div>
-              <div>
-                <Link to='/notnews' className='br-link'>
-                  Press
-                </Link>
-              </div>
-            </div>
-
-            <div className='col-md-6 col-lg-7 mb-4'>
-              <div className='br-footer-item-heading'>Subscription</div>
-              <div>
-                <Link to='/notnews' className='br-link'>
-                  Manage Subscriptions
-                </Link>
-              </div>
-              <div>
-                <Link to='/notnews' className='br-link'>
-                  Podcast
-                </Link>
-              </div>
-              <div>
-                <Link to='/notnews' className='br-link'>
-                  Newsletter
-                </Link>
-              </div>
-            </div>
-
-            <div className='col-sm-2 col-md-2 col-lg-1 mb-4'>
-              <div className='br-footer-item-heading'>Follow</div>
-              <div className='row'>
-                <div className='col-1'>
-                  <Link to='/notnews' className='br-link'>
-                    <FaSquareXTwitter />
-                  </Link>
-                </div>
-                <div className='col-1'>
-                  <Link to='/notnews' className='br-link'>
-                    <FaSquareInstagram />
-                  </Link>
-                </div>
-                <div className='col-1'>
-                  <Link to='/notnews' className='br-link'>
-                    <FaSquareFacebook />
-                  </Link>
-                </div>
-                <div className='col-1'>
-                  <Link to='/notnews' className='br-link'>
-                    <FaSquareYoutube />
-                  </Link>
-                </div>
-                <div className='col-1'>
-                  <Link to='/notnews' className='br-link'>
-                    <FaSquareTumblr />
-                  </Link>
-                </div>
-                <div className='col-1'>
-                  <Link to='/notnews' className='br-link'>
-                    <FaSquarePinterest />
-                  </Link>
-                </div>
-                <div className='col-1'>
-                  <Link to='/notnews' className='br-link'>
-                    <FaSquareReddit />
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className='row pt-3 pb-5 '>
-            <div className='row'>
-              <div className='col-12'>
-                <div className='br-footer-row pt-3'>
-                  <Link to='/notnews' className='br-link br-footer-item'>
-                    Privacy Policy
-                  </Link>
-                  <Link to='/notnews' className='br-link br-footer-item'>
-                    Do Not Sell Or Share My Personal Information
-                  </Link>
-                  <Link to='/notnews' className='br-link br-footer-item'>
-                    Terms & Conditions
-                  </Link>
-                  <Link to='/notnews' className='br-link br-footer-item'>
-                    Terms of Sale
-                  </Link>
-                  <Link to='/notnews' className='br-link br-footer-item'>
-                    Site Map
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
-        </Container>
-      </BrFooter>
+      <BrFooter></BrFooter>
     </>
   );
 }
