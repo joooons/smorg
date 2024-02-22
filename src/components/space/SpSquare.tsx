dbb92ab74401a63bdce11899277429b481d61edf
joooons <52220710+joooons@users.noreply.github.com>
2024-02-21 23:28:14 -0800

    add centered square

---
+++ src/components/space/SpSquare.tsx
@@ -0,0 +1,13 @@
+import './SpSquare.css';
+
+const SpSquare = () => {
+  return (
+    <>
+      <div className='sp-centered'>
+        <div className='sp-square'>Space</div>
+      </div>
+    </>
+  );
+};
+
+export default SpSquare;
--- src/pages/Space.tsx
+++ src/pages/Space.tsx
@@ -1,42 +1,13 @@
-import { useState } from 'react';
-import { Container } from 'react-bootstrap';
-
-import Card from '../components/Card';
 import NavBar from '../components/NavBar';
+import SpSquare from '../components/space/SpSquare';
 
-import Input from '../components/Input';
-import Button from '../components/Button';
-import Text from '../components/Text';
+import '../../src/components/space/Space.css';
 
 function Space() {
-  const [entry, setEntry] = useState('');
-  const handleChange = (event: Event) => {
-    const target = event.target as HTMLInputElement;
-    setEntry(target.value);
-  };
-  const [items, setItems] = useState(['']);
-  const addItem = (text: string) => {
-    setItems([...items, text]);
-  };
-
   return (
     <>
       <NavBar></NavBar>
-      <Container>
-        <Card
-          title='Write something then click on button.'
-          text='This will add a new card.'
-        >
-          <Input myEntry={entry} myMethod={handleChange}></Input>
-          <Button value={entry} action={addItem} followUp={setEntry}>
-            BUTTON
-          </Button>
-        </Card>
-        <Text>This is just a simple text</Text>
-        {items.map((item, index) => {
-          if (item) return <Card key={index} title='' text={item}></Card>;
-        })}
-      </Container>
+      <SpSquare></SpSquare>
     </>
   );
 }
