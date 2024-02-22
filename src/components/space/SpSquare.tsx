284a71d49192d3d0c7fe239bded9e1c5e37a1722
joooons <52220710+joooons@users.noreply.github.com>
2024-02-22 01:09:44 -0800

    add elements with mouse movement

--- src/components/space/SpSquare.tsx
+++ src/components/space/SpSquare.tsx
@@ -8,20 +8,20 @@ interface Props {
 const SpSquare = ({ children }: Props) => {
   const [spots, setSpots] = useState<any>([]);
 
-  const [pos, setPos] = useState<number[]>([0, 0]);
+  //   const [pos, setPos] = useState<number[]>([0, 0]);
 
-  const mousemove = (event: React.MouseEvent<HTMLDivElement>) => {
-    const elem = document.getElementById('sp-square');
-    const rect = elem?.getBoundingClientRect();
-    // const dim = [elem?.offsetWidth, elem?.offsetHeight];
-    const boxPos = [rect?.left, rect?.top];
-    const pos = [
-      boxPos[0] ? event.clientX - boxPos[0] : event.clientX,
-      boxPos[1] ? event.clientY - boxPos[1] : event.clientY,
-    ];
-    // console.log(pos, '   dim: ', dim);
-    setPos(pos);
-  };
+  //   const mousemove = (event: React.MouseEvent<HTMLDivElement>) => {
+  //     const elem = document.getElementById('sp-square');
+  //     const rect = elem?.getBoundingClientRect();
+  //     // const dim = [elem?.offsetWidth, elem?.offsetHeight];
+  //     const boxPos = [rect?.left, rect?.top];
+  //     const pos = [
+  //       boxPos[0] ? event.clientX - boxPos[0] : event.clientX,
+  //       boxPos[1] ? event.clientY - boxPos[1] : event.clientY,
+  //     ];
+  //     // console.log(pos, '   dim: ', dim);
+  //     setPos(pos);
+  //   };
 
   const mouseclick = (event: React.MouseEvent<HTMLDivElement>) => {
     const elem = document.getElementById('sp-square');
@@ -33,7 +33,7 @@ const SpSquare = ({ children }: Props) => {
     ];
     let spotsList = [...spots];
     spotsList.push([pos[0], pos[1]]);
-    if (spotsList.length === 30) {
+    if (spotsList.length === 250) {
       spotsList.shift();
     }
     setSpots(spotsList);
@@ -44,27 +44,28 @@ const SpSquare = ({ children }: Props) => {
         <div
           className='sp-square'
           id='sp-square'
-          onMouseMove={mousemove}
-          onMouseDown={mouseclick}
+          onMouseMove={mouseclick}
+          //   onMouseDown={mouseclick}
         >
           {children}
-          {spots.map((spot: number[]) => {
+          {spots.map((spot: number[], index: number) => {
             return (
               <div
+                key={index}
                 style={{
                   position: 'absolute',
-                  left: `${spot[0] - 20}px`,
-                  top: `${spot[1] - 20}px`,
-                  height: '40px',
-                  width: '40px',
+                  left: `${spot[0] - 10}px`,
+                  top: `${spot[1] - 10}px`,
+                  height: '20px',
+                  width: '20px',
                   borderRadius: '100%',
-                  boxShadow: '0px 0px 50px rgba(255,255,200, 0.5)',
+                  boxShadow: '0px 0px 50px rgba(255,255,200, 0.6)',
                   backgroundColor: 'rgba(255,255,200, 0.05)',
                 }}
               ></div>
             );
           })}
-          <div
+          {/* <div
             style={{
               position: 'absolute',
               left: `${pos[0] - 5}px`,
@@ -74,7 +75,7 @@ const SpSquare = ({ children }: Props) => {
               borderRadius: '100%',
               backgroundColor: 'rgba(255,255,200, 0.5)',
             }}
-          ></div>
+          ></div> */}
         </div>
       </div>
     </>
