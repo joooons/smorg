ee0a73aef739fc7891679cd732fe1cfa70a82533
joooons <joon.s.chang@gmail.com>
2024-03-16 20:00:27 -0700

    add y: gsap anim to HomeLogoBox on hover

--- src/components/home/HomeLogoBox.tsx
+++ src/components/home/HomeLogoBox.tsx
@@ -1,4 +1,6 @@
 import { ReactNode } from 'react';
+import { gsap } from 'gsap';
+
 import './HomeLogoBox.css';
 
 interface Props {
@@ -6,11 +8,29 @@ interface Props {
 }
 
 const HomeLogoBox = ({ children }: Props) => {
+  const moveUp = (ev: React.MouseEvent<HTMLElement>) => {
+    ev.stopPropagation;
+    const target = ev.target as HTMLElement;
+    const grandparent = target.parentNode?.parentNode;
+    grandparent && gsap.to(grandparent, { y: -2, duration: 0.2 });
+  };
+
+  const moveDn = (ev: React.MouseEvent<HTMLElement>) => {
+    ev.stopPropagation;
+    const target = ev.target as HTMLElement;
+    const grandparent = target.parentNode?.parentNode;
+    grandparent && gsap.to(grandparent, { y: 2, duration: 0.2 });
+  };
+
   return (
     <>
       <div className='hm-logobox'>
         <div className='hm-logobox-lid'>
-          <div className='hm-logobox-cover'></div>
+          <div
+            className='hm-logobox-cover'
+            onMouseEnter={moveUp}
+            onMouseLeave={moveDn}
+          ></div>
         </div>
         <div>{children}</div>
       </div>
